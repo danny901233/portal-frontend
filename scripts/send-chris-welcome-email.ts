75d1fb8c2506e1922cef88ab9a41f85ced610a3e
DanTyldesley <dan@DanTyldesleys-MacBook-Air.local>
2026-03-04 17:49:13 +0000

    Fix import path in welcome email script

--- scripts/send-chris-welcome-email.ts
+++ scripts/send-chris-welcome-email.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { sendWelcomeEmail } from '../backend/src/utils/email.js';
+import { sendWelcomeEmail } from '../utils/email.js';
 
 const prisma = new PrismaClient();
 
